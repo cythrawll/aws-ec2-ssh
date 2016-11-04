7974a9b8d9760d715f1fca9bee34c6a064b34313
Chad <cythrawll@codeangel.org>
2016-11-04 16:21:53 -0500

    so that users named @colorjar.com are the only ones that can login.

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -4,6 +4,6 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-aws iam list-ssh-public-keys --user-name "$1" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
-  aws iam get-ssh-public-key --user-name "$1" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
+aws iam list-ssh-public-keys --user-name "$1@colorjar.com" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
+  aws iam get-ssh-public-key --user-name "$1@colorjar.com" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
 done
