3b349ab2d32de5892acf1f919b3a5748ff8bd2bf
Chad <cythrawll@codeangel.org>
2016-11-04 14:07:20 -0500

    Update import_users.sh

--- import_users.sh
+++ import_users.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+aws iam list-users --query "Users[].[UserName]" --output text | while read Email; do
+  User=`echo $Email | sed -e 's/@.*//g'`
+  echo $User
   if id -u "$User" >/dev/null 2>&1; then
     echo "$User exists"
   else
-    /usr/sbin/adduser "$User"
+    /usr/sbin/adduser "$User" --disabled-password -gecos ""
     echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
   fi
 done
